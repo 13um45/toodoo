129785f80e5506a11fb3c558fab8fea23e8bd7ff
13um45 <christian.leumas@icloud.com>
2015-11-01 12:25:03 -0500

    intial commit

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,14 @@
+$(document).on("ready page:load", ready);
+
+function ready() {
+	$('#new_task').on('ajax:success', newTask);
+
+	function newTask() {
+      window.location.reload();
+	}
+}
+
+function newTask(event, data) {
+	$('#tasks').append(data);
+	$('#task_name').val('');
+}
